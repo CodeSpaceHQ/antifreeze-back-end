dfdce5948373f68c1d9189fbe28991d119f63dcb
NilsG-S <nils.griswold.steiner@gmail.com>
2018-02-26 14:41:28 -0600

    Basic server register/unregister ideas

--- main.go
+++ main.go
@@ -2,10 +2,22 @@
 package main
 
 import (
-	"net/http"
+	// "net/http"
+	"fmt"
 )
 
 func main() {
-	http.HandleFunc("/ws")
+	// http.HandleFunc("/ws")
+
+	var test []string
+	fmt.Println(test == nil)
+	test = append(test, "stuff")
+	fmt.Println(test == nil)
+
+	var test2 map[int]string
+	fmt.Println(test2 == nil)
+	test2 = make(map[int]string)
+	fmt.Println(test2 == nil)
+
 	return
 }
--- ws/server.go
+++ ws/server.go
@@ -5,7 +5,7 @@ type Server struct {
 	deviceToUser map[int]string
 	// by email to all connected user clients
 	// when updating, send to all of these
-	emailToUsers map[string][]*user
+	emailToUsers map[string]map[*user]bool
 	register     chan *user
 	unregister   chan *user
 }
@@ -15,8 +15,43 @@ type Server struct {
 func NewServer() *Server {
 	return &Server{
 		deviceToUser: make(map[int]*user),
-		emailToUsers: make(map[string][]*user),
+		emailToUsers: make(map[string]map[*user]bool),
 		register:     make(chan *user),
 		unregister:   make(chan *user),
 	}
 }
+
+func (v *Server) Run() {
+	for {
+		select {
+		case user := <-v.register:
+			if v.emailToUsers[user.email] == nil {
+				v.emailToUsers[user.email] = make(map[*user]bool)
+			}
+
+			v.emailToUsers[user.email][user] = true
+		case user := <-v.unregister:
+			if _, ok := v.emailToUsers[user.email][user]; ok {
+				delete(v.emailToUsers[user.email], user)
+				close(user.send)
+			}
+		}
+	}
+}
+
+func (v *Server) register(user *user) error {
+	if v.emailToUsers[user.email] == nil {
+		v.emailToUsers[user.email] = make(map[*user]bool)
+	}
+
+	v.emailToUsers[user.email][user] = true
+}
+
+func (v *Server) unregister(user *user) error {
+	if _, ok := v.emailToUsers[user.email][user]; ok {
+		delete(v.emailToUsers[user.email], user)
+		close(user.send)
+	}
+}
+
+// Add functions to inject temperatures, devices, etc. into the server.
