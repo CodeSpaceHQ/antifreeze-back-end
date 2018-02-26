93067a3aeb125279f3748ffed8eddf1c9b852363
NilsG-S <nils.griswold.steiner@gmail.com>
2018-02-26 01:07:47 -0600

    Fleshed out user

--- ws/user.go
+++ ws/user.go
@@ -7,11 +7,37 @@ import (
 
 // TODO(NilsG-S): could permissions and subscriptions be their own structs?
 
+type perms struct {
+	authed bool
+}
+
+const (
+	authed *perms = &perms{
+		authed: true,
+	}
+	unauthed *perms = &perms{
+		authed: false,
+	}
+)
+
+type subs struct {
+	userDevices   bool
+	devicesAlarm  bool
+	deviceHistory bool
+}
+
 type user struct {
 	email string
-	// Could potentially make this a set...
-	permissions map[string]bool
+	perms map[string]bool
 	// used to decide whether to send information
 	// technically not necessay under the current proposal
-	subscriptions map[string]bool
+	subs map[string]bool
+	conn *websocket.Conn
+	send chan []byte
+}
+
+// ensures a given user has the right permissions
+func (v *user) checkAuth(req *perms) bool {
+	// TODO: do stuff
+	return true
 }
