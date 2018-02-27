288cb65ecd69e68c53ee3b5892511a370386d078
NilsG-S <nils.griswold.steiner@gmail.com>
2018-02-27 13:20:54 -0600

    Testing connection

--- main.go
+++ main.go
@@ -7,20 +7,39 @@ import (
 	"net/http"
 	"time"
 
+	"github.com/gorilla/websocket"
+
 	"github.com/NilsG-S/antifreeze-back-end/common"
 	"github.com/NilsG-S/antifreeze-back-end/ws"
 )
 
 var addr = flag.String("addr", ":8080", "http service address")
+var upgrader = websocket.Upgrader{
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+}
 
 func main() {
 	server := ws.NewServer()
 	go server.RunServer()
 
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		switch r.Method {
+		case http.MethodGet:
+			http.ServeFile(w, r, "home.html")
+		}
+	})
+
 	http.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
 		switch r.Method {
 		case http.MethodPost:
-			server.Register(w, r)
+			// server.Register(w, r)
+			conn, err := upgrader.Upgrade(w, r, nil)
+			if err != nil {
+				log.Println(err)
+				return
+			}
+			log.Println(conn)
 		}
 	})
 
