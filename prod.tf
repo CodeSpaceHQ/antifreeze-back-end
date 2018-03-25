1d6532ff821861ff64331ef65d40c0e982e1a78c
NilsG-S <nils.griswold.steiner@gmail.com>
2018-03-25 12:02:13 -0500

    Removed extraneous node pool

--- prod.tf
+++ prod.tf
@@ -1,20 +1,13 @@
 provider "google" {
-	# Uses application default credentials
-  project    = "antifreeze-199016"
-  zone       = "us-central1-a"
-}
-
-resource "google_container_node_pool" "np" {
-  name       = "antifreeze-np"
-  zone       = "us-central1-a"
-  cluster    = "${google_container_cluster.c.name}"
-  node_count = 1
+  # Uses application default credentials
+  project = "antifreeze-199016"
+  zone    = "us-central1-a"
 }
 
 resource "google_container_cluster" "c" {
   name               = "antifreeze-c"
   zone               = "us-central1-a"
-	initial_node_count = 1
+  initial_node_count = 1
 
   node_config {
     machine_type = "g1-small"
