7154289ea6758f9f6d22a3edd656ce148468c481
NilsG-S <nils.griswold.steiner@gmail.com>
2018-03-26 00:14:52 -0500

    Removed unnecessary load balancer

--- deploy/terraform/prod.tf
+++ deploy/terraform/prod.tf
@@ -10,51 +10,6 @@ module "kube" {
   }
 }
 
-# TODO: This may be unnecessary for kubernetes
-module "gce-lb-http" {
-  source = "GoogleCloudPlatform/lb-http/google"
-  name   = "antifreeze-lb"
-  ssl    = false
-
-  target_tags = ["${var.target_tags}"]
-
-  url_map        = "${google_compute_url_map.um.self_link}"
-  create_url_map = false
-
-  backends = {
-    "0" = [
-      {
-        group = "${module.kube.instance_group_urls[0]}"
-      },
-    ]
-  }
-
-  backend_params = [
-    "/,http,8081,10",
-  ]
-}
-
-# TODO: This may be unnecessary for kubernetes
-resource "google_compute_url_map" "um" {
-  name            = "antifreeze-um"
-  default_service = "${module.gce-lb-http.backend_services[0]}"
-
-  host_rule = {
-    hosts        = ["*"]
-    path_matcher = "allpaths"
-  }
-
-  path_matcher = {
-    name            = "allpaths"
-    default_service = "${module.gce-lb-http.backend_services[0]}"
-
-    path_rule {
-      paths   = ["/test/post"]
-      service = "${kubernetes_service.kser.metadata.self_link}"
-    }
-  }
-}
-
 # resource "kubernetes_secret" "ksec" {}
 
 resource "kubernetes_service" "kser" {
