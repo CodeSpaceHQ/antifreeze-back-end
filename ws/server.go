a2abae45cfd969af5e104eb040f800788b166f5d
NilsG-S <nils.griswold.steiner@gmail.com>
2018-04-07 08:36:46 -0500

    Fixed WS origin error

--- ws/server.go
+++ ws/server.go
@@ -13,6 +13,10 @@ var (
 	upgrader = websocket.Upgrader{
 		ReadBufferSize:  1024,
 		WriteBufferSize: 1024,
+		// Allow all origins
+		CheckOrigin: func(r *http.Request) bool {
+			return true
+		},
 	}
 )
 
